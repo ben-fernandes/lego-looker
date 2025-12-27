1f5c3287694c3cedf385311f4488cef6c9f338a1
Ben Fernandes <dev.benfernandes@gmail.com>
2025-12-27 23:30:21 +0000

    Initializes basic layout with components
    Sets up the initial application layout by introducing the parts sidebar and sets area components.
    
    This provides the basic structure for managing and displaying LEGO parts and sets. It removes the default React template.

---
+++ src/components/SetsArea.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+
+interface Set {
+  id: string;
+  name: string;
+  partsOwned: number;
+  partsTotal: number;
+}
+
+export default function SetsArea() {
+  const [showIgnored, setShowIgnored] = useState(false);
+  const [sets, setSets] = useState<Set[]>([
+    {
+      id: '1',
+      name: 'Brandenburg Gate',
+      partsOwned: 2,
+      partsTotal: 2,
+    },
+  ]);
+
+  return (
+    <main>
+      <h2>Sets</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Parts Matched</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sets.map((set) => (
+            <tr key={set.id}>
+              <td>
+                <div></div>
+              </td>
+              <td>{set.name}</td>
+              <td>
+                {set.partsOwned}/{set.partsTotal}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <button onClick={() => setShowIgnored(!showIgnored)}>
+        Show ignored sets
+      </button>
+    </main>
+  );
+}
